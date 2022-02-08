0a88925e10944a46bba91541ddd0a72800f29048
Eugooti <eugeneotieno003@gmail.com>
2022-02-08 13:13:55 +0300

    Running well

---
+++ src/main/java/com/mycompany/hostelsystem/MainClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hostelsystem;
+
+/**
+ *
+ * @author 640
+ */
+public class MainClass {
+    
+}
